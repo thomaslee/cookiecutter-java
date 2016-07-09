a80089aa568fa43918607d2bf467def5afc69bd1
Tom Lee <github@tomlee.co>
2016-07-08 21:34:48 -0700

    Rewrite base-java template

---
+++ {{cookiecutter.project_name|lower}}/src/main/java/{{cookiecutter.package_dir}}/Main.java
@@ -0,0 +1,7 @@
+package {{cookiecutter.package}};
+
+public final class Main {
+    public static void main(final String... args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ {{cookiecutter.project_name|lower}}/src/test/java/{{cookiecutter.package_dir}}/MainTest.java
@@ -0,0 +1,11 @@
+package {{cookiecutter.package}};
+
+import org.junit.Test;
+
+public class MainTest {
+    @Test
+    public void canMain() {
+        Main.main();
+    }
+}
+
