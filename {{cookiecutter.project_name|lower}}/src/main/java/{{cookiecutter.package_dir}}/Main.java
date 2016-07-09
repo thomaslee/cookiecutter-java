0010740bcf248482aa7c75986788af8edb5f79e8
Tom Lee <github@tomlee.co>
2016-07-09 00:45:07 -0700

    Logback config

--- {{cookiecutter.project_name|lower}}/src/main/java/{{cookiecutter.package_dir}}/Main.java
+++ {{cookiecutter.project_name|lower}}/src/main/java/{{cookiecutter.package_dir}}/Main.java
@@ -1,7 +1,12 @@
 package {{cookiecutter.package}};
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public final class Main {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
     public static void main(final String... args) {
-        System.out.println("Hello World");
+        LOGGER.info("Hello, World");
     }
 }
